6f2410ed3e59d03fca3093ab0c3d8b079889dbcf
P.T Nyamuda <ptnrlab@gmail.com>
2025-02-24 08:12:32 +0200

    promise added to delete status function

--- src/store/modules/statuses.js
+++ src/store/modules/statuses.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import router from "@/router";
 import { useToast } from "vue-toastification";
-import { resolve } from "core-js/fn/promise";
+import { reject, resolve } from "core-js/fn/promise";
 const toast = useToast();
 //Booking statuses e.g "cancelled", "confirmed", "en route" etc
 const statuses = {
@@ -11,6 +11,7 @@ const statuses = {
     isCreatingStatus: false,
     isUpdatingStatus: false,
     isGettingStatuses: false,
+    isDeletingStatus: false,
   }),
   mutations: {
     //set the car wash statuses
@@ -97,60 +98,32 @@ const statuses = {
       });
     },
     // Delete a status
-    async deleteStatus({ dispatch, rootState }, id) {
-      try {
+    deleteStatus({ dispatch, rootState }, id) {
+      return new Promise((resolve, reject) => {
+        state.isDeletingStatus = true;
         //add authorization header to the request
         //to access the protected route
         dispatch("setAuthorizationHeader");
         // Send a DELETE request to the API
-        let response = await axios.delete(`${rootState.apiUrl}/statuses/${id}`);
-
-        // Check if the request was successful
-        //status code will be 204 from the API
-        if (response.status == 204) {
-          //show toast success message
-          let message = "The status has been successfully deleted.";
-          toast.success(message);
-
-          //refresh the state
-          await dispatch("getStatuses");
-        } else {
-          toast.error(rootState.failureMessage);
-        }
-      } catch (error) {
-        toast.error(rootState.failureMessage);
-      }
+        axios
+          .delete(`${rootState.apiUrl}/statuses/${id}`)
+          .then(() => {
+            let message = "The status has been successfully deleted.";
+            resolve(message);
+          })
+          .catch((ex) => {
+            let message =
+              ex.response?.data?.message || rootState.failureMessage;
+            reject(message);
+          })
+          .finally(() => {
+            state.isDeletingStatus = false;
+          });
+      });
     },
     //mark a task as completed
-    async updateStatus({ dispatch, state, rootState }, payload) {
-      try {
-        let { id, updatedStatus } = payload;
-        state.isUpdatingStatus = true;
-
-        //add authorization header to the request
-        //to access the protected route
-        dispatch("setAuthorizationHeader");
-        //make the request
-        const response = await axios.put(
-          `${rootState.apiUrl}/statuses/${id}`,
-          updatedStatus
-        );
-        // Check if the request was successful
-        //status code will be 204 from the API
-        if (response.status == 204) {
-          //show toast success message
-          let message = "The status has been updated.";
-          toast.success(message);
-
-          router.push("/statuses");
-        } else {
-          toast.error(rootState.failureMessage);
-        }
-      } catch (error) {
-        toast.error(rootState.failureMessage);
-      } finally {
-        state.isUpdatingStatus = false;
-      }
+    updateStatus({ dispatch, state, rootState }, payload) {
+      
     },
 
     //Set authorization header for all request to access protected routes from the API
