4be45055b00d9dab87ec3bc8bf39024b7e85b2ec
P.T Nyamuda <ptnrlab@gmail.com>
2025-03-08 17:20:51 +0200

    padding added to top bar

--- src/components/Layout/NavBarSection.vue
+++ src/components/Layout/NavBarSection.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Topbar Start -->
-  <div class="container-fluid top-bar d-none d-lg-block text-primary">
+  <div class="container-fluid top-bar d-none d-lg-block py-2">
     <div
       class="d-flex align-items-center justify-content-end align-items-center"
     >
