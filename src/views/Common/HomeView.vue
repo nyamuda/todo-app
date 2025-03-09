6974c25f2ebca00b85dfa84aeab973d0cde67b3c
P.T Nyamuda <ptnrlab@gmail.com>
2025-03-09 08:17:25 +0200

    carousel section added to home view

--- src/views/Common/HomeView.vue
+++ src/views/Common/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <CarouselSection />
     <HeroSection />
     <AboutUsSection />
     <ServicesSection />
@@ -10,4 +11,5 @@
 import HeroSection from "@/components/Common/Home/HeroSection.vue";
 import AboutUsSection from "@/components/Common/Home/AboutUsSection.vue";
 import ServicesSection from "@/components/Common/Home/ServicesSection.vue";
+import CarouselSection from "@/components/Common/Home/CarouselSection.vue";
 </script>
