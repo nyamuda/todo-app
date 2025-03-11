bf613070f778ce5f3d77a56955dd5915db56c1fe
P.T Nyamuda <ptnrlab@gmail.com>
2025-03-11 05:45:35 +0200

    about us mb-0 added

--- src/components/Common/Home/AboutUsSection.vue
+++ src/components/Common/Home/AboutUsSection.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="row">
-    <div class="col-md-5"></div>
-    <div class="col-md-7 d-flex flex-column align-items-start">
+    <div class="col-md-6"></div>
+    <div class="col-md-6 d-flex flex-column align-items-start">
       <p class="heading-line rounded"></p>
-      <p class="fs-4 text-capitalize">About us</p>
+      <p class="fs-4 text-capitalize mb-0">About us</p>
+      <h2 class="display-5 fw-bold mb-3">
+        Premium Car Care for a Lasting Shine
+      </h2>
+      <p>
+        Sparkle Auto Wash is a trusted mobile car wash service dedicated to
+        delivering high-quality vehicle cleaning at your convenience. Whether
+        you need a quick wash or full detailing, we bring professional-grade
+        services to your doorstep.
+      </p>
     </div>
   </div>
 </template>
