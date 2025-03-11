f50c5026ac77ddab95192c386c3b1be423e4da67
P.T Nyamuda <ptnrlab@gmail.com>
2025-03-11 06:03:45 +0200

    about us number statistic added

--- src/components/Common/Home/AboutUsSection.vue
+++ src/components/Common/Home/AboutUsSection.vue
@@ -4,7 +4,7 @@
     <div class="col-md-6 d-flex flex-column align-items-start">
       <p class="heading-line rounded"></p>
       <p class="fs-4 text-capitalize mb-0">About us</p>
-      <h2 class="display-5 fw-bold mb-3">
+      <h2 class="display-5 fw-bold mb-4">
         Premium Car Care for a Lasting Shine
       </h2>
       <p>
@@ -13,6 +13,17 @@
         you need a quick wash or full detailing, we bring professional-grade
         services to your doorstep.
       </p>
+      <div class="d-flex align-items-center">
+        <div
+          class="d-flex flex-column align-items-start text-start bg-primary p-3"
+        >
+          <p class="display-3 fw-bold mb-0">50</p>
+          <div class="fs-5 fw-bold text-light">
+            <p class="mb-0">Cars Washed &</p>
+            <p>Shining</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
