c3bbd05c20dda63f7487e6aa522fd176cd88b0b4
P.T Nyamuda <ptnrlab@gmail.com>
2025-03-11 12:31:51 +0200

    homepage statistics cards all added

--- src/components/Common/Home/StatisticsSection.vue
+++ src/components/Common/Home/StatisticsSection.vue
@@ -11,6 +11,39 @@
         </div>
       </div>
     </div>
+    <div class="card col-sm-6 col-md-3 text-light bg-primary">
+      <div class="card-body">
+        <div
+          class="card-text d-flex flex-column align-items-center text-center"
+        >
+          <span><i class="fas fa-car fa-3x"></i></span>
+          <span class="display-4 fw-bold mt-2 mb-1 text-secondary">183</span>
+          <span class="fs-5 fw-bold">Cars Washed</span>
+        </div>
+      </div>
+    </div>
+    <div class="card col-sm-6 col-md-3 text-light bg-primary">
+      <div class="card-body">
+        <div
+          class="card-text d-flex flex-column align-items-center text-center"
+        >
+          <span><i class="fas fa-award fa-3x"></i></span>
+          <span class="display-4 fw-bold mt-2 mb-1 text-secondary">3</span>
+          <span class="fs-5 fw-bold">Years of Service</span>
+        </div>
+      </div>
+    </div>
+    <div class="card col-sm-6 col-md-3 text-light bg-primary">
+      <div class="card-body">
+        <div
+          class="card-text d-flex flex-column align-items-center text-center"
+        >
+          <span><i class="fas fa-star fa-3x"></i></span>
+          <span class="display-4 fw-bold mt-2 mb-1 text-secondary">4.8/5</span>
+          <span class="fs-5 fw-bold">Satisfaction Rating</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
