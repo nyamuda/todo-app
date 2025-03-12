5596bf4c3e7c85e2af364f400dc7aafeb3732467
P.T Nyamuda <ptnrlab@gmail.com>
2025-03-12 10:22:16 +0200

    homepage services section added

--- src/components/Common/Home/ServicesSection.vue
+++ src/components/Common/Home/ServicesSection.vue
@@ -1,64 +1,43 @@
 <template>
   <!-- Services Start -->
-  <div class="pt-5">
-    <div class="container">
-      <div class="text-center pb-2">
-        <h6 class="text-primary text-uppercase font-weight-bold">Features</h6>
-        <h1 class="mb-4">What We Offer</h1>
-      </div>
-      <div class="row pb-3">
-        <div class="col-lg-3 col-md-6 text-center mb-5">
-          <div
-            class="d-flex align-bookings-center justify-content-center bg-primary mb-4 p-4"
-          >
-            <i class="fa fa-2x fa-tasks text-light pe-3"></i>
-            <h6 class="text-white font-weight-medium m-0">Task Management</h6>
-          </div>
-          <p>
-            Easily add and organize your tasks with titles, descriptions, and
-            due dates.
-          </p>
-        </div>
-        <div class="col-lg-3 col-md-6 text-center mb-5">
-          <div
-            class="d-flex align-bookings-center justify-content-center bg-primary mb-4 p-4"
-          >
-            <i class="fa fa-2x fa-calendar-check text-light pe-3"></i>
-            <h6 class="text-white font-weight-medium m-0">Due Date Tracking</h6>
-          </div>
-          <p>
-            Never miss a deadline again. Set due dates for your tasks and get
-            reminders to stay on track.
-          </p>
-        </div>
-        <div class="col-lg-3 col-md-6 text-center mb-5">
-          <div
-            class="d-flex align-bookings-center justify-content-center bg-primary mb-4 p-4"
-          >
-            <i class="fa fa-2x fa-filter text-light pe-3"></i>
-            <h6 class="text-white font-weight-medium m-0">Task Filtering</h6>
-          </div>
-          <p>
-            Filter tasks by completed, uncompleted, or view all. Quickly find
-            what you need and focus on your priorities.
-          </p>
-        </div>
-        <div class="col-lg-3 col-md-6 text-center mb-5">
-          <div
-            class="d-flex align-bookings-center justify-content-center bg-primary mb-4 p-4"
-          >
-            <i class="fa fa-2x fa-spinner text-light pe-3"></i>
-            <h6 class="text-white font-weight-medium m-0">Lazy Loading</h6>
-          </div>
-          <p>
-            Load tasks dynamically as you scroll. Perfect for managing long
-            lists without overwhelming your screen.
-          </p>
-        </div>
+  <div>
+    <div class="text-center">
+      <TitleSection
+        subtitle="Car wash services"
+        title="Our Expertise"
+        align-items="center"
+      />
+      <div class="row mb-4">
+        <p class="col-md-8 m-auto">
+          We offer professional car wash and detailing services to restore your
+          car’s shine and freshness. From hand washes to deep interior cleaning,
+          we do it all.
+        </p>
       </div>
     </div>
   </div>
   <!-- Services End -->
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted, computed } from "vue";
+import TitleSection from "../Elements/TitleSection.vue";
+import { useStore } from "vuex";
+
+let store = useStore();
+let services = computed(() => store.state.services.services);
+onMounted(async () => {
+  try {
+    //fetch all the services
+    await store.dispatch("services/getServices");
+  } catch (ex) {
+    console.log(ex);
+  }
+});
+
+//get only two services to show on the homepage
+let getTwoServices = computed(() => {
+
+
+});
+</script>
