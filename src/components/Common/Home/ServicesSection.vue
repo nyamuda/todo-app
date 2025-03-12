3cc6aee5b1647728bc9ded1504a99cb5b7324348
P.T Nyamuda <ptnrlab@gmail.com>
2025-03-12 10:29:19 +0200

    service list added to homepage services section

--- src/components/Common/Home/ServicesSection.vue
+++ src/components/Common/Home/ServicesSection.vue
@@ -14,6 +14,35 @@
           we do it all.
         </p>
       </div>
+
+      <!-- Service list start -->
+      <!--Service item skeletons-->
+      <div class="row" v-if="isGettingServices">
+        <div
+          v-for="val in serviceItemSkeletons"
+          :key="val"
+          class="col-md-6 mb-4"
+        >
+          <ServiceItemSkeleton />
+        </div>
+      </div>
+      <!--Car wash services-->
+      <div class="row" v-else-if="services.length > 0 && !isGettingServices">
+        <!--Service items-->
+        <div
+          v-for="service in services"
+          :key="service.id"
+          class="col-md-6 mb-4"
+        >
+          <ServiceItem :service="service" />
+        </div>
+      </div>
+      <EmptyList
+        v-else
+        title="No Services Available"
+        message="We're setting up our services. Check back soon."
+      />
+      <!-- Service list end -->
     </div>
   </div>
   <!-- Services End -->
@@ -23,9 +52,15 @@
 import { onMounted, computed } from "vue";
 import TitleSection from "../Elements/TitleSection.vue";
 import { useStore } from "vuex";
+import ServiceItemSkeleton from "./ServiceItemSkeleton.vue";
+import Button from "primevue/button";
+import EmptyList from "../Common/Elements/EmptyList.vue";
+import ServiceItem from "./ServiceItem.vue";
 
 let store = useStore();
 let services = computed(() => store.state.services.services);
+let serviceItemSkeletons = new Array(2);
+let isGettingServices = computed(() => store.state.services.isGettingServices);
 onMounted(async () => {
   try {
     //fetch all the services
@@ -36,8 +71,5 @@ onMounted(async () => {
 });
 
 //get only two services to show on the homepage
-let getTwoServices = computed(() => {
-
-
-});
+let getTwoServices = computed(() => services.value.slice(0, 2));
 </script>
