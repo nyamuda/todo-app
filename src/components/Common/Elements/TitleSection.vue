5610d9f2e248598e1fe8a573d7ab03fbd4458282
P.T Nyamuda <ptnrlab@gmail.com>
2025-03-12 09:01:18 +0200

    text align class added to title section

--- src/components/Common/Elements/TitleSection.vue
+++ src/components/Common/Elements/TitleSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div :class="textAlignClass">
     <p class="heading-line rounded"></p>
     <p class="fs-4 text-capitalize mb-0">{{ subtitle }}</p>
     <h2 class="display-5 fw-bold mb-4">{{ title }}</h2>
@@ -7,12 +7,28 @@
 </template>
 
 <script setup>
-defineProps({
+import { computed } from "vue";
+let props = defineProps({
   title: {
     type: String,
   },
   subtitle: {
     type: String,
   },
+  textAlign: {
+    type: String,
+    default: "start",
+  },
+});
+
+//Class to use to align the items
+let textAlignClass = computed(() => {
+  if (props.textAlign == "center") {
+    return "text-center";
+  } else if (props.textAlign == "end") {
+    return "text-end";
+  } else {
+    return "text-start";
+  }
 });
 </script>
