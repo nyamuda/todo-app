3420b365d0031d0836a30139a8d221af798eb7b5
P.T Nyamuda <ptnrlab@gmail.com>
2025-03-13 16:52:53 +0200

    bootstrap custom theme added

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import "./assets/styles.css";
 
