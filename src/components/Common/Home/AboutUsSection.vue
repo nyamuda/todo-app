81b1e0b53afd083ad196fe54fc9e1fc77492096e
P.T Nyamuda <ptnrlab@gmail.com>
2025-03-14 09:15:10 +0200

    about section color changed to primary

--- src/components/Common/Home/AboutUsSection.vue
+++ src/components/Common/Home/AboutUsSection.vue
@@ -31,9 +31,9 @@
       </p>
       <div class="d-flex align-items-center mb-4">
         <div
-          class="d-flex flex-column align-items-start text-start bg-secondary p-3"
+          class="d-flex flex-column align-items-start text-start bg-primary p-3"
         >
-          <p class="display-3 fw-bold mb-0 text-primary">50</p>
+          <p class="display-3 fw-bold mb-0 text-secondary">50</p>
           <div class="fs-5 fw-bold text-light">
             <p class="mb-0">Cars Washed &</p>
             <p>Shining</p>
@@ -41,19 +41,19 @@
         </div>
         <d class="d-flex flex-column align-items-start gap-3 ms-3">
           <div class="d-flex align-items-center">
-            <i class="fa-solid fa-circle-check me-2 fs-5 text-secondary"></i
+            <i class="fa-solid fa-circle-check me-2 fs-5 text-primary"></i
             ><span>Premium Hand Wash</span>
           </div>
           <div class="d-flex align-items-center">
-            <i class="fa-solid fa-circle-check me-2 fs-5 text-secondary"></i
+            <i class="fa-solid fa-circle-check me-2 fs-5 text-primary"></i
             ><span>Exterior & Interior Detailing</span>
           </div>
           <div class="d-flex align-items-center">
-            <i class="fa-solid fa-circle-check me-2 fs-5 text-secondary"></i
+            <i class="fa-solid fa-circle-check me-2 fs-5 text-primary"></i
             ><span>Leather Conditioning</span>
           </div>
           <div class="d-flex align-items-center">
-            <i class="fa-solid fa-circle-check me-2 fs-5 text-secondary"></i
+            <i class="fa-solid fa-circle-check me-2 fs-5 text-primary"></i
             ><span>Paint Protection & Waxing</span>
           </div>
         </d>
@@ -63,7 +63,7 @@
       >
         <div class="card border-0">
           <div class="d-flex justify-content-start align-items-center">
-            <span class="rounded-circle bg-secondary text-light">
+            <span class="rounded-circle bg-primary text-light">
               <i class="fas fa-phone-flip p-3"></i>
             </span>
             <div
@@ -76,7 +76,7 @@
         </div>
         <div class="card border-0">
           <div class="d-flex justify-content-start align-items-center">
-            <span class="rounded-circle bg-secondary text-light">
+            <span class="rounded-circle bg-primary text-light">
               <i class="fas fa-envelope p-3"></i>
             </span>
             <div
