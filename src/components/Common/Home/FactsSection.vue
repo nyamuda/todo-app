adff84783c2077c761daf975ab4e38c281b8bb32
P.T Nyamuda <ptnrlab@gmail.com>
2025-03-14 09:16:21 +0200

    homepage facts section background changed to secondary

--- src/components/Common/Home/FactsSection.vue
+++ src/components/Common/Home/FactsSection.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="row mt-5 bg-primary p-5 g-3 w-100">
     <div class="col-md-6 col-lg-3">
-      <div class="card text-light bg-primary">
+      <div class="card text-light bg-secondary">
         <div class="card-body">
           <div
             class="card-text d-flex flex-column align-items-center text-center"
           >
             <span><i class="fas fa-users fa-3x"></i></span>
-            <span class="display-4 fw-bold mt-2 mb-1 text-secondary">{{
+            <span class="display-4 fw-bold mt-2 mb-1 text-primary">{{
               companyFacts.totalHappyCustomers
             }}</span>
             <span class="fs-5 fw-bold">Happy customers</span>
@@ -16,13 +16,13 @@
       </div>
     </div>
     <div class="col-md-6 col-lg-3">
-      <div class="card text-light bg-primary">
+      <div class="card text-light bg-secondary">
         <div class="card-body">
           <div
             class="card-text d-flex flex-column align-items-center text-center"
           >
             <span><i class="fas fa-car fa-3x"></i></span>
-            <span class="display-4 fw-bold mt-2 mb-1 text-secondary">{{
+            <span class="display-4 fw-bold mt-2 mb-1 text-primary">{{
               companyFacts.totalCompletedBookings
             }}</span>
             <span class="fs-5 fw-bold">Cars Washed</span>
@@ -37,7 +37,7 @@
             class="card-text d-flex flex-column align-items-center text-center"
           >
             <span><i class="fas fa-award fa-3x"></i></span>
-            <span class="display-4 fw-bold mt-2 mb-1 text-secondary">{{
+            <span class="display-4 fw-bold mt-2 mb-1 text-primary">{{
               companyFacts.totalYearsInService
             }}</span>
             <span class="fs-5 fw-bold">Years of Service</span>
@@ -52,7 +52,7 @@
             class="card-text d-flex flex-column align-items-center text-center"
           >
             <span><i class="fas fa-star fa-3x"></i></span>
-            <span class="display-4 fw-bold mt-2 mb-1 text-secondary">{{
+            <span class="display-4 fw-bold mt-2 mb-1 text-primary">{{
               companyFacts.overallRating
             }}</span>
             <span class="fs-5 fw-bold">Satisfaction Rating</span>
