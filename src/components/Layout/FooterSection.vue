866affcbd9530ac1628df94451bf27ee8e16c4ab
P.T Nyamuda <ptnrlab@gmail.com>
2025-03-20 12:10:09 +0200

    contact details row added to footer

--- src/components/Layout/FooterSection.vue
+++ src/components/Layout/FooterSection.vue
@@ -72,34 +72,23 @@
           <!-- Grid column -->
 
           <!-- Grid column -->
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">Useful links</h6>
-            <p>
-              <a href="#!" class="text-reset">Pricing</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Settings</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Orders</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Help</a>
-            </p>
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
           <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-            <p><i class="fas fa-home me-3"></i>{{ company?.address }}</p>
-            <p>
-              <i class="fas fa-envelope me-3"></i>
-              {{ company?.email }}
-            </p>
-            <p><i class="fas fa-phone me-3"></i>{{ company?.phone }}</p>
+            <div class="d-flex justify-content-start align-items-start gap-2">
+              <div><i class="fas fa-home"></i></div>
+              <div>{{ company?.address }}</div>
+            </div>
+            <div
+              class="d-flex justify-content-start align-items-start gap-2 my-2"
+            >
+              <div><i class="fas fa-phone"></i></div>
+              <div>{{ company?.phone }}</div>
+            </div>
+            <div class="d-flex justify-content-start align-items-start gap-2">
+              <div><i class="fas fa-envelope"></i></div>
+              <div>{{ company?.email }}</div>
+            </div>
           </div>
           <!-- Grid column -->
         </div>
