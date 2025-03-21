c719d2dc78f5b41e4e5bb75b11f8fcc059845229
P.T Nyamuda <ptnrlab@gmail.com>
2025-03-21 09:40:58 +0200

    footer  goToSocialPlatform  added

--- src/components/Layout/FooterSection.vue
+++ src/components/Layout/FooterSection.vue
@@ -2,7 +2,7 @@
   <!-- Footer -->
   <footer class="text-center text-lg-start bg-secondary-light text-light">
     <!-- Section: Links  -->
-    <section class="pb-4">
+    <section class="pb-3">
       <div class="container text-center text-md-start mt-5">
         <!-- Grid row -->
         <div class="row mt-3">
@@ -40,15 +40,21 @@
           <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold mb-4">Business Hours</h6>
-            <div class="d-flex flex-column align-items-start mb-2">
+            <div
+              class="d-flex flex-column align-items-center align-items-md-start mb-2"
+            >
               <span class="text-primary fw-bold">Monday - Friday</span>
               <span>09:00 am - 05:00 pm</span>
             </div>
-            <div class="d-flex flex-column align-items-start mb-2">
+            <div
+              class="d-flex flex-column align-items-center align-items-md-start mb-2"
+            >
               <span class="text-primary fw-bold">Saturday</span>
               <span>09:00 am - 01:00 pm</span>
             </div>
-            <div class="d-flex flex-column align-items-start">
+            <div
+              class="d-flex flex-column align-items-center align-items-md-start mb-3 mb-md-0"
+            >
               <span class="text-primary fw-bold">Sunday</span>
               <span>Closed</span>
             </div>
@@ -81,9 +87,24 @@
             <section
               class="d-flex justify-content-center justify-content-md-start align-items-start gap-2 text-light mt-3"
             >
-              <Button severity="primary" rounded icon="fab fa-facebook-f" />
-              <Button severity="primary" rounded icon="fab fa-instagram" />
-              <Button severity="primary" rounded icon="fab fa-linkedin-in" />
+              <Button
+                @click="goToSocialPlatform"
+                severity="primary"
+                rounded
+                icon="fab fa-facebook-f"
+              />
+              <Button
+                @click="goToSocialPlatform"
+                severity="primary"
+                rounded
+                icon="fab fa-instagram"
+              />
+              <Button
+                @click="goToSocialPlatform"
+                severity="primary"
+                rounded
+                icon="fab fa-linkedin-in"
+              />
             </section>
             <!-- Section: Social media -->
           </div>
@@ -115,6 +136,10 @@ let store = useStore();
 
 let company = computed(() => store.state.company.companyFacts.company);
 let services = computed(() => store.state.services.services);
+
+let goToSocialPlatform = (url) => {
+  window.location.href = url;
+};
 </script>
 
 <style scoped>
